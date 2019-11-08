4086ed514dbc5b0b4b16501ad87d7e9bbd9953cb
Joshua Skootsky <joshua.skootsky@gmail.com>
2019-11-08 15:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
   
+  if (string === whisper(string)) {
+    return "I can't hear you!";
+  }
+  if (string === shout(string)) {
+    return "YES INDEED!";
+  }
 }
